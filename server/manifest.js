ebc890523b0b3729efdc2b0cd14ace07b75d9aa5
Devin Ivy <devin@bigroomstudios.com>
2016-06-28 13:55:06 -0400

    Mount the API at /api

--- server/manifest.js
+++ server/manifest.js
@@ -45,7 +45,8 @@ const manifest = module.exports = {
             plugin: {
                 register: '../lib',
                 options: Config.main
-            }
+            },
+            options: { routes: { prefix: '/api' } }
         }
     ]
 
